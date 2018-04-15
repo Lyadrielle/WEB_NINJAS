5cebabdf4e3704bc2dc77ec555e481266c4cb521
dragonbleapiece <nicolas.cusumano0@gmail.com>
2018-04-15 18:31:15 +0200

    sign in/up

--- server/routes/web.php
+++ server/routes/web.php
@@ -15,11 +15,84 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-$router->get('/Hello', function () use ($router) {
-    return 'Hello Mother Fucker !';
-});
+$router->group(['prefix' => 'api'], function () use ($router) {
 
-$router->get('user/{id}', function ($id) {
-    $result = app('db')->select("SELECT * FROM user WHERE id=1");;
-    return $result[0]->name;
+      $router->post('signin', function (Illuminate\Http\Request $request) {
+          $method = $request->method();
+
+          $result = array();
+
+          if($request->isMethod('post') && $request->has('pseudo') && $request->has('mdp')) {
+
+            $pseudo = $request->input('pseudo');
+            $mdp = $request->input('mdp');
+
+            $check = app('db')->select("SELECT idutilisateur FROM utilisateur WHERE pseudo = :pseudo AND motdepasse = :mdp", ["pseudo" => $pseudo, "mdp" => sha1($mdp)]);
+
+            if(count($check) == 0) {
+              $result['error'] = 'Incorrect Ids';
+              $result['code'] = 2;
+              $result['pseudo'] = $pseudo;
+              return response()->json($result);
+            } else {
+
+              $result['id'] = $check[0]->idutilisateur;
+
+              return redirect()->route('home', $result);
+            }
+
+          } else {
+
+            $result['error'] = 'Forbidden';
+            $result['code'] = 403;
+            return response()->json($result);
+
+          }
+
+      });
+
+      $router->post('signup', function (Illuminate\Http\Request $request) {
+          $method = $request->method();
+
+          $result = array();
+
+          if($request->isMethod('post') && $request->has('pseudo') && $request->has('mdp')) {
+
+            $pseudo = $request->input('pseudo');
+
+            $check = app('db')->select("SELECT pseudo FROM utilisateur WHERE pseudo = :pseudo", ["pseudo" => $pseudo]);
+
+
+            if(count($check) > 0) {
+              $result['error'] = 'Pseudo Already Existing';
+              $result['code'] = 1;
+              $result['pseudo'] = $pseudo;
+              return response()->json($result);
+            }
+
+            $mdp = $request->input('mdp');
+
+            $insert = app('db')->insert("INSERT INTO utilisateur (pseudo, motdepasse) VALUES (:pseudo, :mdp)", ["pseudo" => $pseudo, "mdp" => sha1($mdp)]);
+
+            $check = app('db')->select("SELECT idutilisateur FROM utilisateur WHERE pseudo = :pseudo", ["pseudo" => $pseudo]);
+
+            $result['id'] = (string) $check[0]->idutilisateur;
+
+            return redirect()->route('home', $result);
+
+          } else {
+
+            $result['error'] = 'Forbidden';
+            $result['code'] = 403;
+            return response()->json($result);
+
+          }
+
+      });
+
+      $router->get('user/{id}', ['as' => 'home', function ($id) {
+          $result = app('db')->select("SELECT * FROM utilisateur WHERE idutilisateur=:id", ["id" => $id]);
+          return response()->json($result);
+      }]);
+
 });
