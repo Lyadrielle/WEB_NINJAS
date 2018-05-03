2e013a3c576d6a9ca447cd789d0483911852fc8c
CecilePoucet <funymiette@gmail.com>
2018-05-03 20:56:26 +0200

    add all buttons for skillsblock, need callbacks

--- client/src/components/Skill/index.js
+++ client/src/components/Skill/index.js
@@ -9,29 +9,31 @@ class Skill extends Component {
     super(props)
     this.throwShurikens = this.throwShurikens.bind(this)
     this.state = {
-      strength:false,
+      shurikens:false,
       dissimulation:false,
-      wisdom:false,
-      agility:false,
-      stamina:false
+      reading:false,
+      bodybuilding:false,
+      juggling:false
     }
   }
 
   throwShurikens(){
-    this.setState({strength: true})
+    this.setState({shurikens: true})
     setTimeout(function(){
-      if(this.state.strength === true){
-        setTimeout(function(){this.setState({strength:false})}.bind(this), 3000)
+      if(this.state.shurikens === true){
+        setTimeout(function(){this.setState({shurikens:false})}.bind(this), 3000)
     }}.bind(this), 400) // pour laisser le temps à setState de mettre à jour --> Asynchrone
-
-
-
-
   }
+
+
   render () {
     return (
       <div className='skill'>
-         <Button title = "Lancé de shurikens" disabled = {this.state.strength} callBack={this.throwShurikens}/>
+         <Button title = "Lancé de shurikens" disabled = {this.state.shurikens} callBack={this.throwShurikens}/>
+         <Button title = "Lecture" disabled = {this.state.reading} callBack={this.throwShurikens}/>
+         <Button title = "Dissimulation" disabled = {this.state.dissimulation} callBack={this.throwShurikens}/>
+         <Button title = "Musculation" disabled = {this.state.bodybuilding} callBack={this.throwShurikens}/>
+         <Button title = "Jonglage" disabled = {this.state.juggling} callBack={this.throwShurikens}/>
       </div>
     )
   }
