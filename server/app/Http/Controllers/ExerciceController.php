a4b3af74d5217b03c7baea2e5a568b8181351918
cecilerousset <rousset.cecile13@orange.fr>
2018-05-06 14:48:57 +0200

    ajout actions exercice

--- server/app/Http/Controllers/ExerciceController.php
+++ server/app/Http/Controllers/ExerciceController.php
@@ -45,8 +45,36 @@ class ExerciceController extends Controller
         $competences = array();
 
         switch($action){
-            case 0 :
-              $competences = array(["valeur" => 3, "idnomcompetence" => 0]);
+            case 0 : //manger
+              $competences = array(["valeur" => 10, "idnomcompetence" => 2]);
+            break;
+
+            case 1 : //dormir
+              $competences = array(["valeur" => 10, "idnomcompetence" => 1]);
+            break;
+
+            case 2 : //parler
+              $competences = array(["valeur" => 10, "idnomcompetence" => 3]);
+            break;
+
+            case 3 : //lancer de shuriken (aug. force et agilité, baisse énergie et satiété)
+              $competences = array(["valeur" => 2, "idnomcompetence" => 8] , ["valeur" => 3, "idnomcompetence" => 6] , ["valeur" => -3, "idnomcompetence" => 1] , ["valeur" => -2, "idnomcompetence" => 2]);
+            break;
+
+            case 4 : //lecture (aug. sagesse, baisse vie sociale)
+              $competences = array(["valeur" => 3, "idnomcompetence" => 5] , ["valeur" => -2, "idnomcompetence" => 3]);
+            break;
+
+            case 5 : //dissimulation (aug. dissimulation, baisse vie sociale)
+              $competences = array(["valeur" => 3, "idnomcompetence" => 4] , ["valeur" => -2, "idnomcompetence" => 3]);
+            break;
+
+            case 6 : //musculation (aug. force et endurance, baisse énergie et satiété)
+              $competences = array(["valeur" => 3, "idnomcompetence" => 8] , ["valeur" => 2, "idnomcompetence" => 7] , ["valeur" => -3, "idnomcompetence" => 1] , ["valeur" => -3, "idnomcompetence" => 2]);
+            break;
+
+            case 7 : //jonglage (aug. agilité et endurance, baisse énergie et satiété)
+              $competences = array(["valeur" => 3, "idnomcompetence" => 6] , ["valeur" => 3, "idnomcompetence" => 7] , ["valeur" => -2, "idnomcompetence" => 1] , ["valeur" => -2, "idnomcompetence" => 2]);
             break;
 
             default:
