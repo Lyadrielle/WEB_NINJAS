81897e2d186564feee1ce44a5b83bd175534ddb9
Estelle Guingo <estelle.guingo@gmail.com>
2018-05-09 22:23:27 +0200

    Inventory block + data.json fixed + clean some empty files we don't need

--- client/src/containers/Dashboard/index.js
+++ client/src/containers/Dashboard/index.js
@@ -2,21 +2,25 @@ import React, { Component } from 'react'
 import DashboardBlock from '../DashBoardBlock'
 import Skill from '../../components/Skill'
 import Mission from '../../components/Mission'
+import Inventory from '../../components/Inventory'
 
 import './style.css'
 import * as missions from './missions.json'
-
+import * as data from '../../data.json'
 class Dashboard extends Component {
   displaySkillsBlock() {
-    console.log("dshbd competences")
     return <div><Skill/></div>
   }
 
   displayMissionBlock() {
-    console.log("dshbd miss")
     return missions.map((item, i) =>
-      <div className="mission"><Mission mission={item} /></div>
+      <div className="mission" key = {i}><Mission mission={item} /></div>
     )
+  }
+
+  displayInventoryBlock() {
+    console.log("dshbd Inventory")
+    return <div className="inventory"><Inventory objects={data.ninja.inventory} /></div>
   }
 
   render() {
@@ -24,6 +28,7 @@ class Dashboard extends Component {
       <div className='dashboard-app'>
         <DashboardBlock title="Missions" content={this.displayMissionBlock()} />
         <DashboardBlock title="Compétences" content={this.displaySkillsBlock()}/>
+        <DashboardBlock title="Inventaire" content={this.displayInventoryBlock()}/>
 
       </div>
     )
