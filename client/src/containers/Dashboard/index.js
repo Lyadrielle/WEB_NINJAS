78f1ccbd9308131f4ba5ba50f50e392ddd470f38
Gaëlle Calmels <calmels.gaelle@gmail.com>
2018-05-13 19:19:30 +0200

    Need block add

--- client/src/containers/Dashboard/index.js
+++ client/src/containers/Dashboard/index.js
@@ -2,9 +2,32 @@ import React, { Component } from 'react'
 import DashboardBlock from '../DashBoardBlock'
 import Skill from '../../components/Skill'
 import Mission from '../../components/Mission'
+import Needs from '../../components/Needs'
+import NeedBar from '../../components/NeedBar'
+import Button from '../../components/Button'
 
 import './style.css'
 import * as missions from './missions.json'
+
+const needsMock = {
+  "hunger": {
+    value: 5,
+    action: "eat",
+  },
+  "energy": {
+    value: 100,
+    action: "sleep",
+  },
+  "social": {
+    value: 75,
+    action: "talk",
+  },
+}
+
+const levelMock = {
+  "level": 999,
+  "experience": 37,
+}
 
 class Dashboard extends Component {
   displaySkillsBlock() {
@@ -19,12 +42,43 @@ class Dashboard extends Component {
     )
   }
 
+  displayNeedBlock() {
+    const { level, experience } = levelMock
+
+    return (
+      <div className='need-block'>
+        <div className='level'>
+          <h5>{`level ${level}`}</h5>
+          <NeedBar percentage={experience} color='8e8e8e'/>
+        </div>
+
+        <Needs needs = {needsMock}/>
+
+        <div className = 'actions'>
+          {Object.entries(needsMock).map(([label, need]) => {
+            const { action } = need
+
+            return (
+              <Button
+                title = {action}
+                image = {`./images/needs/${label}.png`}
+                callback={() => {
+                  alert(`Your ninja is ${action}ing`)
+                }}
+              />
+            )
+          })}
+        </div>
+      </div>
+    )
+  }
+
   render() {
     return (
       <div className='dashboard-app'>
+        <DashboardBlock title="needs" content={this.displayNeedBlock()}/>
         <DashboardBlock title="Missions" content={this.displayMissionBlock()} />
         <DashboardBlock title="Compétences" content={this.displaySkillsBlock()}/>
-
       </div>
     )
 }
