4c573e75d42b1e609dceea20bfc6ea2e70f7ae18
Gaëlle Calmels <calmels.gaelle@gmail.com>
2018-05-13 19:19:30 +0200

    Need block add

--- client/src/components/Button/index.js
+++ client/src/components/Button/index.js
@@ -26,15 +26,13 @@ class Button extends Component {
   }
 
   render () {
-    const { callBack } = this.props
-    const { title } = this.props
-    const { image } = this.props
+    const { callBack, title, image } = this.props
 
     return (
       <div>
-         <button className='button' type="button" onClick={ this.disableButton } disabled={this.state.disabled}>
-         {image!=null? <img src = { image } alt="button-image"/> : "" }
-         {" " + title }
+         <button className='button' type='button' onClick={ this.disableButton } disabled={this.state.disabled}>
+         {image != null ? <img src = { image } alt='button-image'/> : ' ' }
+         {' ' + title}
          </button>
       </div>
     )
